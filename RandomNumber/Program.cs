714d37ce25f1f8c8c63e4a6f892c580340ba50cc
Sigrit <sigrit.sults@tktk.ee>
2022-11-23 22:30:29 +0200

    Week4ForWhile

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Random rnd= new Random();
+
+int myRandomNum;
+
+int RandomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    myRandomNum = rnd.Next(0, 11);
+    RandomSum = RandomSum + myRandomNum;
+    Console.WriteLine($"My random number: {myRandomNum}");
+}
+
+Console.WriteLine($"random sum total: {RandomSum}");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true;
+
+while(loopActive)
+{
+    cpuRandom= rnd.Next(1, 4);
+  //  Console.WriteLine($"CPU has generated {cpuRandom}.");
+    Console.WriteLine("Make a guess. Enter a number 1-3:");
+
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if(userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congrats, You won");
+        loopActive = false;
+    }
+    else
+    {
+        Console.WriteLine("Oops, You lost!");
+    }
+
+    Console.WriteLine("Have a nice day!");
+}
